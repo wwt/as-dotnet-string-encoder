025f72492cf4a30487e0aaff1f3cee0eb231ab34
Venkatd1384 <79253936+Venkatd1384@users.noreply.github.com>
2021-02-18 16:55:35 +0530

    Update Encoder.cs

--- Encoder/Encoder.cs
+++ Encoder/Encoder.cs
@@ -6,8 +6,58 @@ namespace Encoder
     {
         public string Encode(string message)
         {
-            //Implement your code here!
-            throw new NotImplementedException();
+            try
+            {
+                // Convert fro upper to lower case...
+                   message = message.ToLower();
+
+                // Reverse Number...
+                   string[] splitNumbers = Regex.Split(message, @"[^\d]"); 
+                   if(splitNumbers.Length > 0)
+                   {
+                       foreach(var splitNumber in splitNumbers)
+                       {
+                           if(!string.IsNullOrEmpty(splitNumber))
+                           {
+                               char[] charArrayNumber = splitNumber.ToCharArray();
+                               Array.Reverse(charArrayNumber);
+                               message = message.Replace(splitNumber, new string(charArrayNumber));
+                           }
+                       }
+                   }
+
+                // Replace Vowels to Numbers...
+                   message = message.Replace("a","1").Replace("e","2").Replace("i","3").Replace("o","4").Replace("u","5");
+
+                // Relace y to Space and Vice-Versa...
+                    var replaceSpaces = message.Split(' ');
+                    if(replaceSpaces.Length > 0)
+                    {
+                        for(int i = 0; i < replaceSpaces.Length; i++)
+                        {
+                            replaceSpaces[i]= replaceSpaces[i].Replace("y", " ");
+                        }
+                        message = string.Join("y", replaceSpaces);
+                    }
+                    else
+                        message.Replace("y"," ");
+
+                // Replace Consontants...
+                    char[] charArrayCons = message.ToCharArray();
+                    for(int i = 0; i < charArrayCons.Length; i++)
+                    {
+                         if(!Char.IsDigit(charArrayCons[i]) && !Char.IsSymbol(charArrayCons[i]) && !Char.IsPunctuation(charArrayCons[i])
+                              && charArrayCons[i] !='y' && charArrayCons[i] !=' ')
+                         {
+                             charArrayCons[i]= (char) (charArrayCons[i] - 1);
+                         }
+                    }
+                 return message = new String(charArrayCons);
+            }
+            catch
+            {
+              throw new NotImplementedException();
+            }
         }
     }
 }
