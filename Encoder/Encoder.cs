96d2653d763b40b3ba462591cf38aca512da8145
nooka.chaitanya@gmail.com <nooka.chaitanya@gmail.com>
2021-02-20 23:41:14 +0530

    Chaitanya - coding assignment

--- Encoder/Encoder.cs
+++ Encoder/Encoder.cs
@@ -1,13 +1,66 @@
-using System;
+using encoder.Extensions;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Encoder
 {
-    public class EncoderProcessor
+    public static class EncoderProcessor
     {
-        public string Encode(string message)
+        static EncoderProcessor()
         {
-            //Implement your code here!
-            throw new NotImplementedException();
+            Mappings.LoadMappings();
+        }
+        public static string Encode(string message)
+        {
+
+            message = message.ToLower();
+
+            string encryptedMessage = EncryptAlphabets(message);
+
+            return EncryptNumbers(message, encryptedMessage);
+        }
+
+        private static string EncryptAlphabets(string message)
+        {
+            var encryptedMessage = string.Empty;
+
+            foreach (char item in message)
+            {
+                char character = item;
+
+                if (character.CheckIfSpaceOrAlphabet())
+                {
+                    character = character.ReturnEncryptedValueForSpaceOrAlphabet();
+                }
+
+                encryptedMessage += character;
+            }
+
+            return encryptedMessage;
+        }
+
+        private static string EncryptNumbers(string message, string encryptedMessage)
+        {
+            var numbersInMessage = Regex.Split(message, @"\D+");
+            foreach (string numberInMessage in numbersInMessage.Where(str => !string.IsNullOrEmpty(str)))
+            {
+                encryptedMessage = encryptedMessage.Replace(numberInMessage, GetReverseOfNumber(numberInMessage));
+            }
+
+            return encryptedMessage;
+        }
+
+        private static string GetReverseOfNumber(string numberInMessage)
+        {
+            var reverseOfNumber = string.Empty;
+            int numberLength = numberInMessage.Length - 1;
+            while (numberLength >= 0)
+            {
+                reverseOfNumber += numberInMessage[numberLength];
+                numberLength--;
+            }
+
+            return reverseOfNumber;
         }
     }
 }
---
+++ Encoder/Mappings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace encoder.Extensions
+{
+    public static class Mappings
+    {
+
+        private const char Space = ' ';
+
+        private static readonly IDictionary<char, char> AlphabetMappings = new Dictionary<char, char>();
+
+
+        public static void LoadMappings()
+        {
+            //loading the character mappings from text file
+            string _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            var mappings = File.ReadLines(_filePath + "\\CharacterMappings.txt");
+
+            foreach (var mapping in mappings)
+            {
+                var charactermapping = mapping.Split(',');
+                var key = charactermapping[0].ToCharArray();
+                var value = charactermapping[1].ToCharArray();
+                if (key.Length == 1 && value.Length == 1)
+                    AlphabetMappings.Add(key[0], value[0]);
+            }
+        }
+
+        public static bool CheckIfSpaceOrAlphabet(this char character)
+        {
+            return character == Space || character >= 97 && character <= 122;
+        }
+
+        public static char ReturnEncryptedValueForSpaceOrAlphabet(this char character)
+        {
+            return AlphabetMappings.ContainsKey(character) ? AlphabetMappings[character] : '\0';
+        }
+    }
+}
+
+
