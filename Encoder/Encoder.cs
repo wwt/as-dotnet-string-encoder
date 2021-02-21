59fc2bd4e5e14aef474ee8bf603af624ea77bfbc
harshalwanare <harshalwanare11@gmail.com>
2021-02-21 20:23:53 +0530

    Code Commit For String Encoder

--- Encoder/Encoder.cs
+++ Encoder/Encoder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace Encoder
 {
@@ -6,8 +7,54 @@ namespace Encoder
     {
         public string Encode(string message)
         {
-            //Implement your code here!
-            throw new NotImplementedException();
+            //Reverse numbers present in the message
+            string outputWithReverseNumbers = message.ReverseNumberFromString();
+
+            StringBuilder lowercaseOutput = new StringBuilder();
+
+            lowercaseOutput.Append(outputWithReverseNumbers.ToLower());
+
+            for (int i = 0; i < lowercaseOutput.Length; i++)
+            {
+                switch (lowercaseOutput[i])
+                {
+                    //replace a with 1
+                    case 'a':
+                        lowercaseOutput[i] = '1';
+                        break;
+                    //replace e with 2
+                    case 'e':
+                        lowercaseOutput[i] = '2';
+                        break;
+                    //replace i with 3
+                    case 'i':
+                        lowercaseOutput[i] = '3';
+                        break;
+                    //replace o with 4
+                    case 'o':
+                        lowercaseOutput[i] = '4';
+                        break;
+                    //replace u with 5
+                    case 'u':
+                        lowercaseOutput[i] = '5';
+                        break;
+                    //replace y with space
+                    case 'y':
+                        lowercaseOutput[i] = ' ';
+                        break;
+                    //replace space with y
+                    case ' ':
+                        lowercaseOutput[i] = 'y';
+                        break;
+                    default:
+                        //check if character is alphabet
+                        if (lowercaseOutput[i].IsAlphabet())
+                            lowercaseOutput[i] = (char)(lowercaseOutput[i] - 1);
+                        break;
+                }
+            }
+
+            return lowercaseOutput.ToString();
         }
     }
 }
---
+++ Encoder/HelperUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Encoder
+{
+    public static class HelperUtilities
+    {
+        public static bool IsAlphabet(this char alphabet)
+        {
+            return (alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z');
+        }
+
+        public static string ReverseNumberFromString(this string inputData)
+        {
+            MatchCollection numbers = Regex.Matches(inputData, @"\d+");
+
+            StringBuilder lowercaserOutput = new StringBuilder(inputData);
+
+            foreach (Match item in numbers)
+            {
+                char[] charArray = item.Value.ToCharArray();
+                Array.Reverse(charArray);
+                lowercaserOutput.Replace(item.Value, new string(charArray));
+            }
+
+            return lowercaserOutput.ToString();
+        }
+    }
+}
